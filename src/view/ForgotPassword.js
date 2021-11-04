d31357b935ccee1c44c426a05ab00fa9bc2c1cd5
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-04 22:20:34 +0530

    [ui] auth forgot password back button ui added

--- src/view/ForgotPassword.js
+++ src/view/ForgotPassword.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ForgotPassword = () => {
   return (
@@ -16,7 +17,11 @@ const ForgotPassword = () => {
               className="input input-bordered"
             />
           </div>
-
+          <label className="label">
+            <Link to="/login" className="label-text-alt">
+              Login in account
+            </Link>
+          </label>
           <div className="form-control mt-6">
             <input
               type="button"
