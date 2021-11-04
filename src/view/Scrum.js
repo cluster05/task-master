e7d377734a1e81ad324188abf74cb2baf6c30744
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-04 14:02:39 +0530

    remove predefind ui

--- src/view/Scrum.js
+++ src/view/Scrum.js
@@ -1,23 +1,7 @@
 import React from "react";
-import { Switch, Route } from "react-router";
-import Scrumify from "./Scrum/Scrumify";
-import Sidebar from "../components/Sidebar/Sidebar";
 
 const Scrum = () => {
-  return (
-    <div className="w-screen h-screen flex">
-      <div className="w-16 border-r-2">
-        <Sidebar />
-      </div>
-      <div className="w-full">
-        <Switch>
-          <Route path="/">
-            <Scrumify />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Scrum;
--- src/view/Scrum/Scrumify.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Scrumify = () => {
-  return <div>Hello scrumify</div>;
-};
-
-export default Scrumify;
