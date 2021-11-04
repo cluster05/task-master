6a7426e5fc6d4d81cf0b4dbbf28f980498f02529
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-04 14:47:44 +0530

    daisy ui added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  // purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +9,23 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#498afb",
+          "primary-focus": "#2674fa",
+          "primary-content": "#ffffff",
+          secondary: "#ff4088",
+          "secondary-focus": "#ff649e",
+          "secondary-content": "#ffffff",
+          info: "#2094f3",
+          success: "#0be586",
+          warning: "#fb9865",
+          error: "#ff3860",
+        },
+      },
+    ],
+  },
+};
