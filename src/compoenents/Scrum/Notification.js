98ffebef03f1ffe45461a179076706eab28a6cc3
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-06 18:08:10 +0530

    nofitication zindex added to 50

--- src/compoenents/Scrum/Notification.js
+++ src/compoenents/Scrum/Notification.js
@@ -4,7 +4,7 @@ const Notification = () => {
   const notifications = [];
 
   return (
-    <div className="w-96 bg-white  border shadow absolute top-12 right-0">
+    <div className="w-96 bg-white  border shadow absolute top-12 right-0 z-40">
       <title className="py-2 bg-gray-100 text-sm block">Notifications</title>
       <div className="text-left overflow-y-auto" style={{ maxHeight: "30rem" }}>
         {notifications.map((notification) => (
