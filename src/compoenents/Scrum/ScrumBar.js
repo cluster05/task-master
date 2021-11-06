c857fecbd25f587d5cefa5f0546c68a335449016
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-06 17:43:39 +0530

    Scrum row and card updated

--- src/compoenents/Scrum/ScrumBar.js
+++ src/compoenents/Scrum/ScrumBar.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { FiMoreHorizontal } from "react-icons/fi";
+import ScrumCard from "./ScrumCard";
 
-const ScrumBar = () => {
-  return <div></div>;
+const ScrumBar = ({ title, color }) => {
+  const numberofTask = Math.round(Math.random() * 10 + 1);
+  const tasks = [];
+  for (let i = 0; i < numberofTask; i++) {
+    tasks.push("title" + i);
+  }
+
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <span className={`w-3 h-3 rounded-full bg-${color}`}></span>
+          <h3 className="mx-2"> {title} </h3>
+          <button className="btn btn-square bg-gray-300 hover:bg-gray-300 border-none text-gray-500 btn-xs">
+            {numberofTask}
+          </button>
+        </div>
+        <span className="text-xl">
+          <FiMoreHorizontal />
+        </span>
+      </div>
+      <div>
+        {tasks.map((task, index) => (
+          <ScrumCard key={`${title}-${index}`} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ScrumBar;
--- src/compoenents/Scrum/ScrumCard.js
+++ src/compoenents/Scrum/ScrumCard.js
@@ -1,7 +1,68 @@
 import React from "react";
-
+import { MdAccessAlarms } from "react-icons/md";
+import { FiCheckCircle, FiPaperclip } from "react-icons/fi";
 const ScrumCard = () => {
-  return <div></div>;
+  return (
+    <div className="my-2 p-4 rounded border bg-white text-sm">
+      <div className="cursor-pointer">
+        <title className="block capitalize ">The Future of React Router</title>
+        <p className="mt-1 text-gray-400 text-xs leading-5">
+          We are bringing together the best of React Router and Reach Router
+          into a new, hook-based API. React Router will be the surviving
+          project.
+        </p>
+      </div>
+      <div>
+        <div className="mt-2 flex justify-between items-center text-sm">
+          <span className="text-gray-400 text-xs"> 50 of 100 % </span>
+          <div className="px-1 rounded text-warning border border-warning bg-yellow-50 flex items-center">
+            <span>
+              <MdAccessAlarms />
+            </span>
+            <span className="ml-1">2 W left</span>
+          </div>
+        </div>
+        <progress
+          class="progress progress-success h-1 "
+          value="50"
+          max="100"
+        ></progress>
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img
+            className="h-8 w-8 border border-white rounded-full"
+            src="https://via.placeholder.com/150"
+            alt="image_1"
+          />
+          <img
+            className="h-8 w-8 border border-white rounded-full"
+            src="https://via.placeholder.com/150"
+            alt="image_2"
+          />
+          <img
+            className="h-8 w-8 border border-white rounded-full"
+            src="https://via.placeholder.com/150"
+            alt="image_3"
+          />
+        </div>
+        <div className="flex items-center text-2xs">
+          <div className="px-2 m-1 flex items-center rounded bg-gray-200 text-gray-400">
+            <span className="mr-1">
+              <FiPaperclip />
+            </span>
+            <span>6</span>
+          </div>
+          <div className="px-2 m-1 flex items-center rounded bg-gray-200 text-gray-400">
+            <span className="mr-1">
+              <FiCheckCircle />
+            </span>
+            <span>1 / 8</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ScrumCard;
