fe033546781620a5cf31539557d56b72c85fe7bd
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-06 09:08:54 +0530

    sidebar added for scrum app

--- src/compoenents/Scrum/Sidebar.js
+++ src/compoenents/Scrum/Sidebar.js
@@ -1,7 +1,37 @@
 import React from "react";
-
+import {
+  FiBarChart2,
+  FiFolder,
+  FiFileText,
+  FiCalendar,
+  FiMessageSquare,
+  FiSettings,
+} from "react-icons/fi";
+import { BiTask } from "react-icons/bi";
+import { Link } from "react-router-dom";
 const Sidebar = () => {
-  return <div></div>;
+  const sidebarMenu = [
+    { title: "Dashboard", icon: <FiBarChart2 />, link: "dashboard" },
+    { title: "Projects", icon: <BiTask />, link: "projects" },
+    { title: "Tasks", icon: <FiFolder />, link: "tasks" },
+    { title: "Documents", icon: <FiFileText />, link: "documents" },
+    { title: "Calender", icon: <FiCalendar />, link: "calender" },
+    { title: "Messages", icon: <FiMessageSquare />, link: "messages" },
+    { title: "Settings", icon: <FiSettings />, link: "settings" },
+  ];
+
+  return (
+    <div className="h-screen w-56 py-1 pr-5">
+      {sidebarMenu.map((sidebarItem) => (
+        <Link to={`/app/${sidebarItem.link}`} key={sidebarItem.title}>
+          <div className="w-full my-3 px-4 py-2 bg-gray-100 rounded-tr-full cursor-pointer rounded-br-full  flex items-center">
+            <span className="mx-3 text-lg"> {sidebarItem.icon} </span>
+            <span className="m-1 text-sm"> {sidebarItem.title} </span>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Sidebar;
