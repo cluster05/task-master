cbba73a06a72a7e86c90e67e83c1e8bf9229b0de
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-06 16:14:32 +0530

    notifiaction bar added in navbar

--- src/compoenents/Scrum/Navbar.js
+++ src/compoenents/Scrum/Navbar.js
@@ -1,7 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FiSearch, FiUser, FiBell, FiHelpCircle } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import Notification from "./Notification";
 const Navbar = () => {
-  return <div></div>;
+  const [showNotification, setShowNotification] = useState(false);
+
+  const toggleShowNotification = () => {
+    setShowNotification(!showNotification);
+  };
+
+  return (
+    <div className="m-2 flex justify-between items-center">
+      <div className="p-1 border flex items-center bg-white rounded-lg">
+        <span className="ml-2  text-xl">
+          <FiSearch />
+        </span>
+        <input
+          className="px-4 py-2 text-sm focus:outline-none"
+          placeholder="Search"
+        />
+      </div>
+      <div>
+        <button
+          className="m-1 p-3 bg-white border rounded-lg relative"
+          onClick={toggleShowNotification}
+        >
+          <FiBell />
+          {showNotification && <Notification />}
+        </button>
+        <button className="m-1 p-3 bg-white border rounded-lg">
+          <FiHelpCircle />
+        </button>
+        <Link to="/app/account">
+          <button className="m-1 p-3 bg-white border rounded-lg">
+            <FiUser />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/compoenents/Scrum/Notification.js
+++ src/compoenents/Scrum/Notification.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Notification = () => {
-  return <div></div>;
+  const notifications = [];
+
+  return (
+    <div className="w-96 bg-white  border shadow absolute top-12 right-0">
+      <title className="py-2 bg-gray-100 text-sm block">Notifications</title>
+      <div className="text-left overflow-y-auto" style={{ maxHeight: "30rem" }}>
+        {notifications.map((notification) => (
+          <div className="text-sm m-2 py-2 border-b" key={notification.id}>
+            <p> {notification.timestamp} </p>
+            <title className="block capitalize">{notification.title}</title>
+            <p className="text-gray-400 text-2xs">
+              {" "}
+              {notification.description}{" "}
+            </p>
+          </div>
+        ))}
+        {notifications.length == 0 && (
+          <div className="py-5 capitalize text-center text-gray-300">
+            no new notification
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Notification;
