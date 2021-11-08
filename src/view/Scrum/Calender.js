790c2af5f9d4a21faef9fe9ae875092a90ebf359
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-08 22:26:00 +0530

    calender ui added

--- src/view/Scrum/Calender.js
+++ src/view/Scrum/Calender.js
@@ -10,6 +10,180 @@ const Calender = () => {
       />
       <div className="mb-10 grid grid-cols-3">
         <div className="col-span-2 px-4 ">
+          <div className="px-1 mt-10">
+            <h3>
+              <span className="font-medium">Monday</span>, Novembar 8
+            </h3>
+            <div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-warning">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-warning h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-warning">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-warning h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-success">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-success h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-error">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-error h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+            </div>
+          </div>
+          <div className="px-1 mt-10">
+            <h3>
+              <span className="font-medium">Monday</span>, Novembar 8
+            </h3>
+            <div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-warning">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-warning h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-warning">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-warning h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-success">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-success h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+              <div className="mt-4 pt-4 border-t-2 flex justify-between items-center">
+                <p className="whitespace-nowrap">
+                  <span className="font-medium">9:00</span> - 10:00
+                </p>
+                <div className="mx-4 px-4 border-l-4 border-error">
+                  <title className="block capitalize ">
+                    The Future of React Router
+                  </title>
+                  <p className="mt-1 text-gray-500 text-xs leading-5">
+                    We are bringing together the best of React Router and Reach
+                    Router into a new. We are bringing together the best of
+                    React Router and Reach Router into a new.
+                  </p>
+                </div>
+                <progress
+                  className="progress progress-error h-1 w-1/4 "
+                  value="50"
+                  max="100"
+                ></progress>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="border-l">
         </div>
