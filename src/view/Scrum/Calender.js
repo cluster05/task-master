0c6db9bbae276664588959a3b280c468f635a1ff
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-08 22:25:37 +0530

    grid ui added

--- src/view/Scrum/Calender.js
+++ src/view/Scrum/Calender.js
@@ -8,6 +8,12 @@ const Calender = () => {
         className=" input text-sm bg-gray-200 text-gray-500 focus:shadow-none rounded-full"
         type="date"
       />
+      <div className="mb-10 grid grid-cols-3">
+        <div className="col-span-2 px-4 ">
+        </div>
+        <div className="border-l">
+        </div>
+      </div>
     </div>
   );
 };
