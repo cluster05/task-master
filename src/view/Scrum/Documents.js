a1b83e8c888b3aec4313a0cfdc50963176fca4a7
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-13 21:58:45 +0530

    document ui added

--- src/view/Scrum/Documents.js
+++ src/view/Scrum/Documents.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "../../compoenents/Scrum/Header";
+import { FiStar, FiSearch } from "react-icons/fi";
 
 const Documents = () => {
   return (
     <div>
       <Header title="Documents"></Header>
+      <div>
+        <div> </div>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="p-3 rounded shadow-md bg-white"></div>
+        </div>
+      </div>
     </div>
   );
 };
